29a05669faf92c981ea4fa07b16e63b0903edebb
rajatkumar59 <rajatkumar8102002@gmail.com>
2025-02-05 16:23:55 +0530

    final commit

--- Weather-Forecast-App-in-HTML-CSS-and-JavaScript/app.js
+++ Weather-Forecast-App-in-HTML-CSS-and-JavaScript/app.js
@@ -1,6 +1,6 @@
 let moreBtn = document.querySelector(".more-btn");
 let input = document.querySelector("input");
-const key = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+let key = "f814c3568077ab90ef5cb875c677116a";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
 
 // Toggle more info section
